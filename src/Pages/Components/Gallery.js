e1b8e8c487b76f01ad3588ca127e947f2f0c5efb
kissan-beej-bhandar <sttarworld01@gmail.com>
2021-12-09 22:15:38 +0530

    All Done

--- src/Pages/Components/Gallery.js
+++ src/Pages/Components/Gallery.js
@@ -16,31 +16,31 @@ const Gallery = () => {
                     <div className="selction-gallery-1">
                         <div className="left-gallery-top">
                             <div className="left-top-left-photo">
-                                <img src="media/img2.jpg" onClick={()=>gallery('img2.jpg')} alt="gallery-img" className="gallery-photo-1" />
+                                <img src="https://kissan-beej-bhandar.github.io/ashok/media/img2.jpg" onClick={()=>gallery('img2.jpg')} alt="gallery-img" className="gallery-photo-1" />
                             </div>
                             <div className="left-top-right-photo">
-                                <img src="media/img1.jpg" onClick={()=>gallery('img1.jpg')} alt="gallery-img" className="gallery-photo-1" />
+                                <img src="https://kissan-beej-bhandar.github.io/ashok/media/img1.jpg" onClick={()=>gallery('img1.jpg')} alt="gallery-img" className="gallery-photo-1" />
                             </div>
                         </div>
                         <div className="left-gallery-bottom">
                             <div className="left-bottom-photo">
-                                <img src="media/img3.jpg" onClick={()=>gallery('img3.jpg')}  alt="gallery-img" className="gallery-photo-1" />
+                                <img src="https://kissan-beej-bhandar.github.io/ashok/media/w2.jpg" onClick={()=>gallery('w2.jpg')}  alt="gallery-img" className="gallery-photo-1" />
                             </div>
                         </div>
                     </div>
                     <div className="selction-gallery-2">
                         <div className="left-gallery-bottom margin-1">
                             <div className="left-bottom-photo">
-                                <img src="media/img6.jpg" onClick={()=>gallery('img6.jpg')} alt="gallery-img" className="gallery-photo-1" />
+                                <img src="https://kissan-beej-bhandar.github.io/ashok/media/w1.jpg" onClick={()=>gallery('w1.jpg')} alt="gallery-img" className="gallery-photo-1" />
                             </div>
                         </div>
                         {/* class="img-collection " */}
                         <div className="left-gallery-top">
                             <div className="left-top-left-photo">
-                                <img src="media/img4.jpg" onClick={()=>gallery('img4.jpg')} alt="gallery-img" className="gallery-photo-1" />
+                                <img src="https://kissan-beej-bhandar.github.io/ashok/media/img4.jpg" onClick={()=>gallery('img4.jpg')} alt="gallery-img" className="gallery-photo-1" />
                             </div>
                             <div className="left-top-right-photo d2">
-                                <img src="media/img5.jpg" onClick={()=>gallery('img5.jpg')} alt="gallery-img" className="gallery-photo-1" />
+                                <img src="https://kissan-beej-bhandar.github.io/ashok/media/w4.jpg" onClick={()=>gallery('w4.jpg')} alt="gallery-img" className="gallery-photo-1" />
                             </div>
                         </div>
                     </div>
